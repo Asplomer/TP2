1e756b3ba207d8f934516b01c8e47e0bc2ce8416
Ariel Plomer <asplomer@gmail.com>
2020-11-09 14:19:33 -0300

    añadido otro nivel de prueba
    tambien modificado para que se pueda acceder a los 2 niveles de prueba que hice con los botones test2 y test3, play te lleva a game que no funciona actualmente

--- Race Track/scenes/levels/menu.gd
+++ Race Track/scenes/levels/menu.gd
@@ -10,11 +10,22 @@ func _ready():
 
 
 func _on_Play_pressed():
-	#get_tree().change_scene("res://scenes/levels/game.tscn")
-	get_tree().change_scene("res://scenes/test2.tscn")
+	get_tree().change_scene("res://scenes/levels/game.tscn")
+	
+	
 	pass 
 
 
 func _on_Exit_pressed():
 	get_tree().quit()
 	pass 
+
+
+func _on_to_Test2_pressed():
+	get_tree().change_scene("res://scenes/test2.tscn")
+	pass # Replace with function body.
+
+
+func _on_To_Test3_pressed():
+	get_tree().change_scene("res://scenes/test3.tscn")
+	pass # Replace with function body.
