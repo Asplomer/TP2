ab47fe33a7d611838807a66f0ec09932f683a686
Ariel Plomer <asplomer@gmail.com>
2020-11-13 19:57:37 -0300

    Arreglado el codigo
    ahora cambia de escena... hasta que llega al nivel "3" donde explota, si se ponen mas niveles o menos match deberia funcionar bien

--- Race Track/scenes/levels/gamemanager.gd
+++ Race Track/scenes/levels/gamemanager.gd
@@ -8,35 +8,35 @@ var CurrentInstance = level1.instance()
 
 
 func _ready():
-	self.add_child(CurrentInstance)
-	get_tree().call_group("Goals", "connect", "detected", self, "_next_level")
+	call_deferred("_add_level")
+	
 	pass 
-
-
-
 
 func _process(delta):
 	
 	pass
 
 func _next_level():
-
+		get_tree().call_group("Goals", "disconnect", "detected", self, "_next_level")
 		CurrentInstance.queue_free()
 		match level_counter:
 			0:
 				CurrentInstance = level2.instance()
 				level_counter +=1
-				self.add_child(CurrentInstance)
+				call_deferred("_add_level")
 			1:
 				CurrentInstance = level3.instance()
 				level_counter +=1
-				self.add_child(CurrentInstance)
-				
+				call_deferred("_add_level")
 			2: 
 				level_counter +=1
-				self.add_child(CurrentInstance)
+				call_deferred("_add_level")
 				#CurrentInstance = level4.instance()
 			3:
 				get_tree().change_scene("res://scenes/levels/menu.tscn")
-	
+ 
 pass
+ 
+func _add_level():
+	self.add_child(CurrentInstance)
+	get_tree().call_group("Goals", "connect", "detected", self, "_next_level")
