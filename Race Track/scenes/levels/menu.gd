5030ca4587237c57b075918bd675292534fdd6b3
Ariel Plomer <asplomer@gmail.com>
2020-11-19 00:33:19 -0300

    cambiado el nivel 1 y sacado los botones para testear niveles
    el nivel 1 era el que venia con race track originalmente y me parecio una cagada. aparte no funcionan las señales para llamarlo desde otro lado

--- Race Track/scenes/levels/menu.gd
+++ Race Track/scenes/levels/menu.gd
@@ -20,17 +20,17 @@ func _on_Exit_pressed():
 	get_tree().quit()
 	pass 
 
-
-func _on_to_Test2_pressed():
-	get_tree().change_scene("res://scenes/test2.tscn")
-	pass # Replace with function body.
-
-
-func _on_To_Test3_pressed():
-	get_tree().change_scene("res://scenes/test3.tscn")
-	pass # Replace with function body.
-
-
-func _on_Button5_pressed():
-	get_tree().change_scene("res://scenes/test4.tscn")
-	pass # Replace with function body.
+#
+#func _on_to_Test2_pressed():
+#	get_tree().change_scene("res://scenes/test2.tscn")
+#	pass # Replace with function body.
+#
+#
+#func _on_To_Test3_pressed():
+#	get_tree().change_scene("res://scenes/test3.tscn")
+#	pass # Replace with function body.
+#
+#
+#func _on_Button5_pressed():
+#	get_tree().change_scene("res://scenes/test4.tscn")
+#	pass # Replace with function body.
