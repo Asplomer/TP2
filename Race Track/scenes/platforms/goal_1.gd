07b846adde378e85f07b4519fb9f06443cc0171f
Nacho <ignacio.pourteau@gmail.com>
2020-11-19 01:42:00 -0300

    Mas cambios para lo de High Score

---
+++ Race Track/scenes/platforms/goal_1.gd
@@ -0,0 +1,16 @@
+extends StaticBody2D
+
+signal detected
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("wheels"):
+		VariablesGlobales.best_time_1(currentTime)
+		emit_signal("detected")
+	pass 
